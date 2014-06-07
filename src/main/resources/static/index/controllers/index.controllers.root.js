67006a06271c81b39906f77c34503092843ac563
vvasianovych <vvasianovych@simplefeed.com>
2014-06-07 21:00:23 +0300

    moving forward

--- src/main/resources/static/index/controllers/index.controllers.root.js
+++ src/main/resources/static/index/controllers/index.controllers.root.js
@@ -1,3 +1,6 @@
 angular.module('index.module.rootApp')
 .controller('index.controllers.root', [ '$scope', '$location', function($scope, $location) {
+    $scope.login = function() {
+        alert('login:'+$scope.currentUser.login+"::"+$scope.currentUser.password );
+    };
 } ]);
